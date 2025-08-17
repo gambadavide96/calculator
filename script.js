f2d5b5d6f3246a88768d536fbbe56004c49ac166
Davide Gamba <gamba.davide1996@gmail.com>
2025-08-17 11:43:35 +0200

    Adding separator and change sign functions

--- script.js
+++ script.js
@@ -1,82 +1,80 @@
 
 const display = document.querySelector(".display")
+let numDigits = 0; //count numDigits in the expressions (max 9)
 
-//Evitare duplicazioni di codice, scrivi una funzione per gestire gli inserimenti
-//TODO, aggiungere logica per inserire il punto ogni 3 numeri
+//Adding . as separator like iOS calculator
+const addSeparator = () => {
+  let cleaned = display.textContent.replace(/\./g, ""); // clean the string from .
+  let [intPart, decPart] = cleaned.split(",");          // split integer from decimal part
+  intPart = intPart.replace(/\B(?=(\d{3})+(?!\d))/g, "."); // adding separator
+  display.textContent = decPart ? `${intPart},${decPart}` : intPart; //recombining string
+}
+
+const addDigit = function(digit) {
+
+  //Gestisco il caso in cui era stato inserito uno zero iniziale
+  if(display.textContent === "0"){
+    display.textContent = digit;
+    return
+  }
+
+  if(numDigits < 10){
+    display.textContent += digit;
+    numDigits += 1;
+    addSeparator();
+  }
+}
+
 
 const btn0 = document.querySelector("#digit-0");
-btn0.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "0";
-  }
+btn0.addEventListener("click",() => {
+  //Per evitare di inserire zero ripetuti all'inizio
+  if (display.textContent === "0")
+    return
+  addDigit("0");
 });
 
 const btn1 = document.querySelector("#digit-1");
-btn1.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "1";
-  }
-});
+btn1.addEventListener("click",() => addDigit("1"));
 
 const btn2 = document.querySelector("#digit-2");
-btn2.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "2";
-  }
-});
+btn2.addEventListener("click",() => addDigit("2"));
 
 const btn3 = document.querySelector("#digit-3");
-btn3.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "3";
-  }
-});
+btn3.addEventListener("click",() => addDigit("3"));
 
 const btn4 = document.querySelector("#digit-4");
-btn4.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "4";
-  }
-});
+btn4.addEventListener("click",() => addDigit("4"));
 
 const btn5 = document.querySelector("#digit-5");
-btn5.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "5";
-  }
-});
+btn5.addEventListener("click",() => addDigit("5"));
 
 const btn6 = document.querySelector("#digit-6");
-btn6.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "6";
-  }
-});
+btn6.addEventListener("click",() => addDigit("6"));
 
 const btn7 = document.querySelector("#digit-7");
-btn7.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "7";
-  }
-});
+btn7.addEventListener("click",() => addDigit("7"));
 
 const btn8 = document.querySelector("#digit-8");
-btn8.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "8";
-  }
-});
+btn8.addEventListener("click",() => addDigit("8"));
 
 const btn9 = document.querySelector("#digit-9");
-btn9.addEventListener("click", () => {
-  if(display.textContent.length < 9){
-    display.textContent += "9";
-  }
-});
+btn9.addEventListener("click",() => addDigit("9"));
 
 const btnClear = document.querySelector("#clear");
 btnClear.addEventListener("click", () => {
   display.textContent = ''
+  numDigits = 0;
+})
+
+const btnChangeSign = document.querySelector("#changeSign");
+btnChangeSign.addEventListener("click", () => {
+  let arr = display.textContent.split('');
+  if (!arr.includes("-"))
+    arr.splice(0,0,"-");
+  else
+    arr.splice(0,1);
+  display.textContent = arr.join('');
 })
 
 
